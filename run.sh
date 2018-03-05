e490cb4c88c1e931dfe1588bbc4cbb94d709d40c
prakhar <prakhar@u.nus.edu>
2018-03-05 15:35:38 +0800

    Modifying the script to run all dockers, adding mongodb dockerfile

--- run.sh
+++ run.sh
@@ -6,7 +6,12 @@ if [ "$EUID" -ne 0 ]
 fi
 
 TEAMID=`md5sum README.md | cut -d' ' -f 1`
+web="$TEAMID""web"
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
-docker build . -t $TEAMID
-docker run -p 80:80 -p 8080:8080 -t $TEAMID
+docker build mean-docker/websec -t "$TEAMID""web"
+docker run -d -p 80:80 -t "$TEAMID""web"
+docker build mean-docker/mongo -t "$TEAMID""mon"
+docker run -d -p 27017:27017 -t "$TEAMID""mon"
+docker build mean-docker/backend -t $TEAMID
+docker run -p 8080:8080 -t $TEAMID
